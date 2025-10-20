153cdf4b3227e9590b07fde04c93c7e5dab73edd
joosti93 <joosthettinga@gmail.com>
2025-10-20 15:17:04 +0200

    movement completed and groups created

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000     
+        updatable_group.update(dt)
         screen.fill((0, 0, 0))
-        player.update(dt)
+        for drawable in drawable_group:
+            drawable.draw(screen)
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
-        player.draw(screen)
+        print(f"Screen height: {SCREEN_HEIGHT}")    
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
