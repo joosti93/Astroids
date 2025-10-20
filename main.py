4b48f75831a9c67e526210a5ce19fc9fd2a8410a
joosti93 <joosthettinga@gmail.com>
2025-10-20 14:20:32 +0200

    toevoegen circleshape en triangle

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
