5112dbabe23bf5a66b08fe079fa04a56bea5b0e7
joosti93 <joosthettinga@gmail.com>
2025-10-23 10:02:45 +0200

    added asteroids, fields and collision

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    updatable_group = pygame.sprite.Group()
-    drawable_group = pygame.sprite.Group()
-    Player.containers = (updatable_group, drawable_group)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000     
-        updatable_group.update(dt)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collide_with(player):
+                print("Game over!")
+                pygame.quit()
         screen.fill((0, 0, 0))
-        for drawable in drawable_group:
-            drawable.draw(screen)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")    
